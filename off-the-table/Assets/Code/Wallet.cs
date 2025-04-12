73fa77cf33efdefa2b043d8b5a06940b8eb071a3
mie <marie.trojan@uni-weimar.de>
2025-04-12 15:49:51 +0200

    hola amigo

---
+++ off-the-table/Assets/Code/Wallet.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wallet", menuName = "Scriptable Objects/Wallet")]
+public class Wallet : ScriptableObject {
+
+    public int money = 2;
+
+    
+    
+}
