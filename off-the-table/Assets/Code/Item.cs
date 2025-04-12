8b0f036302ccb98cf2972661edcb7bc2b4cc2d79
fionn-the-goose <fionn-the-goose>
2025-04-12 13:27:23 +0000

    eeeeeeyyy

--- off-the-table/Assets/Code/Item.cs
+++ off-the-table/Assets/Code/Item.cs
@@ -1,29 +1,16 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-
-public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
-    private SpriteRenderer sprite;
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        sprite.color = new Color32(255, 255, 255, 170);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = Input.mousePosition;
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        sprite.color = new Color32(255, 255, 255, 255);
-    }
-
+public class Item : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
