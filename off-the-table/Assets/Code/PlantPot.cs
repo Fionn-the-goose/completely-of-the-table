b315e3c501e534a4fffa2f3a88007e7fa6a97d60
fionn-the-goose <fionn-the-goose>
2025-04-12 14:48:53 +0000

    stuffie

--- off-the-table/Assets/Code/PlantPot.cs
+++ off-the-table/Assets/Code/PlantPot.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlantPot : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    private Plant plantInfo;
+    [SerializeField] private SpriteRenderer Showcase;
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Item>()){
+
+        }
+    }
 }
